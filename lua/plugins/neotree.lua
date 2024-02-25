d8c30a62fb4a72ce6809964d3df52cbe21445d4c
Yashodhan Ketkar <kykyashodhan@gmail.com>
2024-02-25 08:23:12 +0530

    changed neotree config
    - neotree will be closed automatically
    - also updated .gitignore wrt theme related commit

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,13 +1,28 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
-    "MunifTanjim/nui.nvim",
-  },
-  keys = {
-    { "<C-n>", ":Neotree filesystem reveal left<cr>", desc = "Open Neotree on right side" },
-    { "<leader>bf", ":Neotree buffers reveal float<cr>", desc = "Shows Neotree buffers" },
-  },
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function(_)
+						-- auto close
+						require("neo-tree.command").execute({
+							action = "close",
+						})
+					end,
+				},
+			},
+		})
+	end,
+	keys = {
+		{ "<C-n>", ":Neotree filesystem reveal right<cr>", desc = "Open Neotree on right side" },
+		{ "<leader>bf", ":Neotree buffers reveal float<cr>", desc = "Shows Neotree buffers" },
+	},
 }
