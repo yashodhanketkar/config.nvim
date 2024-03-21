908599a8a31de4bd84b29e89566d22bd5f40880b
Yashodhan Ketkar <kykyashodhan@gmail.com>
2024-03-21 16:13:09 +0530

    refactor and update
    - Added rainabow csv plugin
    - Placed csv and bats file to nonlsp directory
    - Disabled CSV related options in treesitter
    - Removed repeated commands from ui.lua file
    - Added ufo-folds plugin for fold management

--- lua/config/ui.lua
+++ lua/config/ui.lua
@@ -1,16 +1,14 @@
 local function setupui()
-  vim.cmd("set expandtab")
-  vim.cmd("set number relativenumber")
-  vim.cmd("set tabstop=2")
-  vim.cmd("set softtabstop=2")
-  vim.cmd("set shiftwidth=2")
-  vim.cmd("set smartindent")
-  vim.cmd("set autoindent")
-  vim.cmd("set mouse=a")
-  vim.cmd("set foldenable")
-  vim.cmd("set foldmethod=manual")
+	vim.cmd("set expandtab")
+	vim.cmd("set number relativenumber")
+	vim.cmd("set tabstop=2")
+	vim.cmd("set softtabstop=2")
+	vim.cmd("set shiftwidth=2")
+	vim.cmd("set smartindent")
+	vim.cmd("set autoindent")
+	vim.cmd("set mouse=a")
 end
 
 return {
-  setup = setupui
+	setup = setupui,
 }
---
+++ lua/plugins/folds.lua
@@ -0,0 +1,15 @@
+return {
+	"kevinhwang91/nvim-ufo",
+	dependencies = { "kevinhwang91/promise-async" },
+	config = function()
+		vim.o.foldcolumn = "1"
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		local ufo = require("ufo")
+		vim.keymap.set("n", "zR", ufo.openAllFolds)
+		vim.keymap.set("n", "zM", ufo.closeAllFolds)
+		ufo.setup()
+	end,
+}
