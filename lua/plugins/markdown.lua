19aea3d747036400f2c762c4be4bb9a3063ea24d
Yashodhan Ketkar <kykyashodhan@gmail.com>
2024-10-29 23:37:23 +0530

    minor update
    - changed harpoons append method as per new change append() --> add()
    - added markdown plugins
    - above plugin not added in README yet (not tested on linux yet)
    - dependencies update

---
+++ lua/plugins/markdown.lua
@@ -0,0 +1,23 @@
+return {
+	"iamcco/markdown-preview.nvim",
+	cmd = {
+		"MarkdownPreviewToggle",
+		"MarkdownPreview",
+		"MarkdownPreviewStop",
+	},
+	-- build = function()
+	-- 	vim.fn["mkdp#util#install"]()
+	-- end,
+	build = "cd app && npm install",
+	init = function()
+		vim.g.mkdp_filetypes = {
+			"markdown",
+		}
+	end,
+	ft = {
+		"markdown",
+	},
+	keys = {
+		{ "<leader>mp", "<cmd>MarkdownPreviewToggle<cr>", desc = "Toggle Markdown Preview" },
+	},
+}
